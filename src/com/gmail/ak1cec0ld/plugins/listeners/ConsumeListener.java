582c01dd89873198da8f11a40ab0e0d15a555030
Jeremy Luchak <jeremy@nevada.unr.edu>
2017-03-10 00:26:49 -0800

    NPE check for consume

--- src/com/gmail/ak1cec0ld/plugins/listeners/ConsumeListener.java
+++ src/com/gmail/ak1cec0ld/plugins/listeners/ConsumeListener.java
@@ -18,9 +18,11 @@ public class ConsumeListener implements Listener{
     public void onConsume(PlayerItemConsumeEvent event){
         Player player = event.getPlayer();
         if (player.hasPermission("pokeitems.berries.use")){
-            String item_lore_0 = event.getItem().getItemMeta().getLore().get(0); //NPE Check needed
-            if (plugin.getConfigManager().getValidBerriesWithColors().contains(item_lore_0) && plugin.getConfigManager().getBerryUses(item_lore_0).contains("consume")){
-                plugin.executeBerry(event, player, event.getItem());
+            if (event.getItem().hasItemMeta() && event.getItem().getItemMeta().hasLore()){
+                String item_lore_0 = event.getItem().getItemMeta().getLore().get(0);
+                if (plugin.getConfigManager().getValidBerriesWithColors().contains(item_lore_0) && plugin.getConfigManager().getBerryUses(item_lore_0).contains("consume")){
+                    plugin.executeBerry(event, player, event.getItem());
+                }
             }
         }
     }
